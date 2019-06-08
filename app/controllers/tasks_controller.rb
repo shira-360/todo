55411e166c47b366351eeb9f3612a333736e35af
EC2 Default User <ec2-user@ip-172-31-33-64.ap-southeast-1.compute.internal>
2019-06-08 09:47:20 +0000

    jsの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,10 +9,25 @@ class TasksController < ApplicationController
     
     def create
         Task.create(task_params)
+        redirect_to controller: :tasks, action: :index
+    end
+    
+    def destroy
+        task = Task.find(params[:id])
+        task.destroy
+    end
+    
+    def edit
+        @task = Task.find(params[:id])
+    end
+    
+    def update
+        task = Task.find(params[:id])
+        task.update(task_params)
     end
     
     private
     def task_params
-        params.require(:task).permit(:text)
+        params.require(:task).permit(:title)
     end
 end
